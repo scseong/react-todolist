d8347affbe37d72a3862be769d7e0f8c86badd32
scseong <goidtv@naver.com>
2022-12-20 18:28:03 +0900

    initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const Router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+]);
+
+export default Router;
